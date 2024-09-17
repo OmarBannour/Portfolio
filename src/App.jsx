f0fc63096fe0e21868bcfbf0b4fccf4f79549398
omar banour <bannouromar54@gmail.com>
2024-09-17 19:46:52 +0100

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Componant/Navbar/Navbar';  // Correct the path if necessary
+import Hero from './Componant/Hero/Hero';
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero/>
     </div>
   );
 }
---
+++ src/Componant/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Hero.css"
+import pic from'../../assets/omarr.jpg'
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={pic} alt="" className='pic' />
+        <h1><span>I am Omar Bannour</span> , Full-Stack Developer and IoT Solutions Architect based in Tunisia </h1>
+        <p> I am a full stack developer from Tunisia,Monastir </p>
+        <div className="hero-action">
+            <div className="connect-btn">Connect with me </div>
+            <div className="resume-btn">My resume</div>
+        </div>
+      
+    </div>
+  )
+}
+import "./Hero.css"
+export default Hero
